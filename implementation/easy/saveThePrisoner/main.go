d922147f8164cbe472c28bc1593a0828d8aa685a
Paul Bentley <paulbentley73@gmail.com>
2016-11-17 21:53:05 +0000

    cloud jumping and prisoners

---
+++ implementation/easy/saveThePrisoner/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+// https://www.hackerrank.com/challenges/save-the-prisoner
+func runTest(prisonerCount, sweetCount, startingPrisoner int, debug bool) int {
+	// Start at 1, with 2 sweets
+	// 1, 2
+	//    -
+
+	// Start at 1, with 5 sweets
+	// 1, 2, 3, 4, 5
+	//             -
+
+	// Start at 1, with 6 sweets
+	// 1, 2, 3, 4, 5, 1
+	//                -
+
+	poisoned := (startingPrisoner + sweetCount - 1) % prisonerCount
+	if poisoned == 0 {
+		return prisonerCount
+	}
+	return poisoned
+}
+
+func main() {
+	filenames := []string{"T1.txt"}
+	var start time.Time
+	for fi, fn := range filenames {
+		f, opened, scanner := openFile(fn)
+
+		if opened {
+			defer f.Close()
+			if fi > 0 {
+				fmt.Println()
+			}
+			fmt.Printf("File: %s\n", fn)
+		}
+		if opened {
+			start = time.Now()
+		}
+		testCount := nextInt(scanner)
+		for testIndex := 0; testIndex < testCount; testIndex++ {
+			prisonerCount := nextInt(scanner)
+			sweetCount := nextInt(scanner)
+			startingPrisoner := nextInt(scanner)
+			fmt.Printf("%d\n", runTest(prisonerCount, sweetCount, startingPrisoner, opened))
+		}
+		if opened {
+			elapsed := time.Since(start)
+			log.Printf("Test took: %s\n", elapsed)
+		}
+		if !opened {
+			return
+		}
+	}
+}
+
+func openFile(filename string) (*os.File, bool, *bufio.Scanner) {
+	file, err := os.Open(filename)
+	var opened = true
+	if err != nil {
+		file = os.Stdin
+		opened = false
+	}
+
+	scanner := bufio.NewScanner(file)
+	buf := make([]byte, 0, 64*1024)
+	scanner.Buffer(buf, 1024*1024)
+	scanner.Split(bufio.ScanWords)
+
+	return file, opened, scanner
+}
+
+func nextInt(scanner *bufio.Scanner) int {
+	scanner.Scan()
+	word := scanner.Text()
+	num, _ := strconv.ParseInt(word, 10, 32)
+	return int(num)
+}
+
+func nextInt64(scanner *bufio.Scanner) int64 {
+	scanner.Scan()
+	word := scanner.Text()
+	num, _ := strconv.ParseInt(word, 10, 64)
+	return int64(num)
+}
+
+func nextUInt64(scanner *bufio.Scanner) uint64 {
+	scanner.Scan()
+	word := scanner.Text()
+	num, _ := strconv.ParseUint(word, 10, 64)
+	return num
+}
+
+func nextWord(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	word := scanner.Text()
+	return word
+}
+
+func nextIntArray(scanner *bufio.Scanner, elementCount int) []int {
+	elements := make([]int, 0, elementCount)
+	for elementCount > 0 {
+		elements = append(elements, nextInt(scanner))
+		elementCount--
+	}
+
+	return elements
+}
+
+func absInt(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
