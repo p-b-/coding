4aad827ba14d9b8a926e4c565f1aa9d75b582fca
p-b- <paulbentley73@gmail.com>
2016-11-28 16:37:45 +0000

    Woc 26 day 1

---
+++ contests/woc26/d1/main.go
@@ -0,0 +1,129 @@
+package main // https://www.hackerrank.com/contests/w26/challenges/game-with-cells
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+func runTest(m int, n int, debug bool) int {
+	if debug {
+		fmt.Printf("%d * %d\n", m, n)
+	}
+	if m == 0 || n == 0 {
+		return 0
+	}
+	x := m / 2
+	y := n / 2
+
+	if m%2 != 0 {
+		x++
+	}
+	if n%2 != 0 {
+		y++
+	}
+
+	return x * y
+}
+
+func main() {
+	filenames := []string{"T1.txt", "T2.txt", "T3.txt", "T4.txt", "T5.txt", "T6.txt", "T7.txt", "T8.txt"}
+	var start time.Time
+	for fi, fn := range filenames {
+		f, fileOpened, scanner := openFile(fn)
+
+		if fileOpened {
+			defer f.Close()
+			if fi > 0 {
+				fmt.Println()
+			}
+			fmt.Printf("File: %s\n", fn)
+		}
+		if fileOpened {
+			start = time.Now()
+		}
+
+		m := nextInt(scanner)
+		n := nextInt(scanner)
+		fmt.Printf("%d\n", runTest(m, n, fileOpened))
+		if fileOpened {
+			elapsed := time.Since(start)
+			log.Printf("Test took: %s\n", elapsed)
+		}
+		if !fileOpened {
+			return
+		}
+	}
+}
+
+func openFile(filename string) (*os.File, bool, *bufio.Scanner) {
+	file, err := os.Open(filename)
+	var opened = true
+	if err != nil {
+		file = os.Stdin
+		opened = false
+	}
+
+	scanner := bufio.NewScanner(file)
+	buf := make([]byte, 0, 64*1024)
+	scanner.Buffer(buf, 1024*1024)
+	scanner.Split(bufio.ScanWords)
+
+	return file, opened, scanner
+}
+
+func nextInt(scanner *bufio.Scanner) int {
+	scanner.Scan()
+	word := scanner.Text()
+	num, _ := strconv.ParseInt(word, 10, 32)
+	return int(num)
+}
+
+func nextInt64(scanner *bufio.Scanner) int64 {
+	scanner.Scan()
+	word := scanner.Text()
+	num, _ := strconv.ParseInt(word, 10, 64)
+	return int64(num)
+}
+
+func nextUInt64(scanner *bufio.Scanner) uint64 {
+	scanner.Scan()
+	word := scanner.Text()
+	num, _ := strconv.ParseUint(word, 10, 64)
+	return num
+}
+
+func nextWord(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	word := scanner.Text()
+	return word
+}
+
+func nextIntArray(scanner *bufio.Scanner, elementCount int) []int {
+	elements := make([]int, 0, elementCount)
+	for elementCount > 0 {
+		elements = append(elements, nextInt(scanner))
+		elementCount--
+	}
+
+	return elements
+}
+
+func nextUInt64Array(scanner *bufio.Scanner, elementCount int) []uint64 {
+	elements := make([]uint64, 0, elementCount)
+	for elementCount > 0 {
+		elements = append(elements, nextUInt64(scanner))
+		elementCount--
+	}
+
+	return elements
+}
+
+func absInt(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
+}
